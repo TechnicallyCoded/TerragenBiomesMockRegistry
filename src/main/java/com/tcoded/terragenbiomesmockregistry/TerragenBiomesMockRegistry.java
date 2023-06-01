57ab312e3f794e5f2e4cd9e11627d538e9bf8cbe
TechnicallyCoded <technicallycoded@gmail.com>
2023-06-02 00:42:11 +0200

    Refactor names

--- src/main/java/com/tcoded/terragenbiomesmockregistry/TerragenBiomesMockRegistry.java
+++ src/main/java/com/tcoded/terragenbiomesmockregistry/TerragenBiomesMockRegistry.java
@@ -1,30 +1,20 @@
 package com.tcoded.terragenbiomesmockregistry;
 
-import net.minecraft.core.MappedRegistry;
-import net.minecraft.core.Registry;
-import net.minecraft.core.WritableRegistry;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.server.dedicated.DedicatedServer;
-import net.minecraft.world.level.biome.Biome;
-import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_19_R3.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.terraform.main.TLogger;
 import org.terraform.main.TerraformGeneratorPlugin;
 import org.terraform.v1_19_R3.CustomBiomeHandler;
 
 import java.io.File;
-import java.lang.reflect.Field;
-import java.util.Optional;
 
 public final class TerragenBiomesMockRegistry extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        doMagic2();
+        registerBiomes();
     }
 
-    private void doMagic2() {
+    private void registerBiomes() {
         File pluginsFolder = getDataFolder().getParentFile();
         File terraFolder = new File(pluginsFolder, "TerraformGenerator");
 
@@ -36,28 +26,28 @@ public final class TerragenBiomesMockRegistry extends JavaPlugin {
         CustomBiomeHandler.init();
     }
 
-    private void doMagic() throws NoSuchFieldException, IllegalAccessException {
-        CraftServer craftserver = (CraftServer) Bukkit.getServer();
-        DedicatedServer dedicatedserver = craftserver.getServer();
-
-        Optional<Registry<Biome>> registry = dedicatedserver.registryAccess().registry(Registries.BIOME);
-        if (registry.isPresent()) {
-            getLogger().info("Biome registry is present");
-        } else {
-            getLogger().info("Biome registry is not present");
-            return;
-        }
-
-        Registry<Biome> biomeRegistry = registry.get();
-        MappedRegistry<Biome> writableRegistry = (MappedRegistry<Biome>) biomeRegistry;
-
-        Class<? extends WritableRegistry> writableRegistryClass = writableRegistry.getClass();
-        Field frozenField = writableRegistryClass.getDeclaredField("frozen");
-        frozenField.setAccessible(true);
-        frozenField.set(writableRegistry, false);
-
-        biomeRegistry.freeze();
-    }
+//    private void doMagic() throws NoSuchFieldException, IllegalAccessException {
+//        CraftServer craftserver = (CraftServer) Bukkit.getServer();
+//        DedicatedServer dedicatedserver = craftserver.getServer();
+//
+//        Optional<Registry<Biome>> registry = dedicatedserver.registryAccess().registry(Registries.BIOME);
+//        if (registry.isPresent()) {
+//            getLogger().info("Biome registry is present");
+//        } else {
+//            getLogger().info("Biome registry is not present");
+//            return;
+//        }
+//
+//        Registry<Biome> biomeRegistry = registry.get();
+//        MappedRegistry<Biome> writableRegistry = (MappedRegistry<Biome>) biomeRegistry;
+//
+//        Class<? extends WritableRegistry> writableRegistryClass = writableRegistry.getClass();
+//        Field frozenField = writableRegistryClass.getDeclaredField("frozen");
+//        frozenField.setAccessible(true);
+//        frozenField.set(writableRegistry, false);
+//
+//        biomeRegistry.freeze();
+//    }
 
     @Override
     public void onDisable() {
