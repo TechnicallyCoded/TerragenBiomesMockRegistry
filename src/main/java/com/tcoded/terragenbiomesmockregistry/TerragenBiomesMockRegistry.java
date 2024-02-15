fe4a192cad5600186b32b6e9685d2bb00ed8abec
HSGamer <huynhqtienvtag@gmail.com>
2024-02-15 16:55:29 +0700

    use reflection

--- src/main/java/com/tcoded/terragenbiomesmockregistry/TerragenBiomesMockRegistry.java
+++ src/main/java/com/tcoded/terragenbiomesmockregistry/TerragenBiomesMockRegistry.java
@@ -3,9 +3,10 @@ package com.tcoded.terragenbiomesmockregistry;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.terraform.main.TLogger;
 import org.terraform.main.TerraformGeneratorPlugin;
-import org.terraform.v1_20_R3.CustomBiomeHandler;
+import org.terraform.utils.version.Version;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 public final class TerragenBiomesMockRegistry extends JavaPlugin {
 
@@ -23,31 +24,28 @@ public final class TerragenBiomesMockRegistry extends JavaPlugin {
         }
 
         TerraformGeneratorPlugin.logger = new TLogger();
-        CustomBiomeHandler.init();
-    }
+        String version = Version.getVersionPackage();
 
-//    private void doMagic() throws NoSuchFieldException, IllegalAccessException {
-//        CraftServer craftserver = (CraftServer) Bukkit.getServer();
-//        DedicatedServer dedicatedserver = craftserver.getServer();
-//
-//        Optional<Registry<Biome>> registry = dedicatedserver.registryAccess().registry(Registries.BIOME);
-//        if (registry.isPresent()) {
-//            getLogger().info("Biome registry is present");
-//        } else {
-//            getLogger().info("Biome registry is not present");
-//            return;
-//        }
-//
-//        Registry<Biome> biomeRegistry = registry.get();
-//        MappedRegistry<Biome> writableRegistry = (MappedRegistry<Biome>) biomeRegistry;
-//
-//        Class<? extends WritableRegistry> writableRegistryClass = writableRegistry.getClass();
-//        Field frozenField = writableRegistryClass.getDeclaredField("frozen");
-//        frozenField.setAccessible(true);
-//        frozenField.set(writableRegistry, false);
-//
-//        biomeRegistry.freeze();
-//    }
+        Class<?> biomeHandle;
+        try {
+            biomeHandle = Class.forName("org.terraform." + version + ".CustomBiomeHandler");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to find CustomBiomeHandler for version " + version);
+        }
+
+        Method initMethod;
+        try {
+            initMethod = biomeHandle.getDeclaredMethod("init");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to find init method for CustomBiomeHandler");
+        }
+
+        try {
+            initMethod.invoke(null);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to invoke init method for CustomBiomeHandler");
+        }
+    }
 
     @Override
     public void onDisable() {
